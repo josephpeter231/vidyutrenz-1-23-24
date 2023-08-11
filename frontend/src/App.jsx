a875ad117b4da2ac1be12cd98f62a321e44b3865
Thanush19 <that.thanush.the.bloggy@gmail.com>
2023-08-11 08:55:59 +0530

    tailwind issue fixed , Routes for all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import Events from './pages/Events';
+import ContactUs from './pages/ContactUs';
 
 function App() {
-
   return (
     <>
-      <h1>React App</h1>
-
-    
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={ <AboutUs/>} />
+          <Route path="/events" element={<Events/>} />
+          <Route path="/contactus" element={<ContactUs/>} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
