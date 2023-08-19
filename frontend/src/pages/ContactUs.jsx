7c9277b37d00f95ed756342a98838be85430d6f0
josephpeter231 <josephpeterjcit@gmail.com>
2023-08-19 17:05:13 +0530

    contact js

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import  { useState } from 'react';
+import emailjs from 'emailjs-com';  
 
 const ContactUs = () => {
-  return (
-    <div>ContactUs</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
-export default ContactUs
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send email using EmailJS
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', formData, 'YOUR_USER_ID')
+      .then((response) => {
+        console.log('Email sent successfully:', response);
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+      });
+  };
+
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row items-center justify-center bg-gray-100">
+      <div className="bg-white p-4 rounded-lg shadow-md w-full md:w-2/3 lg:w-1/2 xl:w-1/3 md:mr-4">
+        <h2 className="text-2xl font-semibold mb-4">Contact Us</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="mt-1 p-2 border rounded w-full"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="mt-1 p-2 border rounded w-full"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows="4"
+              value={formData.message}
+              onChange={handleChange}
+              className="mt-1 p-2 border rounded w-full"
+              required
+            />
+          </div>
+          <div className="mt-4">
+            <button
+              type="submit"
+              className="p-8px text-black bg-blue-500 hover:bg-blue-700  font-bold py-2 px-4 rounded"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="md:flex-shrink-0 md:w-1/3 lg:w-1/4 xl:w-1/5 mt-4 md:mt-0">
+      <iframe
+      title="Google Maps"
+      width="100%"
+      height="100%"
+      frameBorder="10"
+      style={{ border: 0 }}
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.0006663305403!2d80.04093371482203!3d12.971808890855769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52f4d07355bab5%3A0xbb6063169c4ed4d9!2sChennai%20Institute%20of%20Technology!5e0!3m2!1sen!2sin!4v1651592789112!5m2!1sen!2sin"
+      allowFullScreen
+    ></iframe>
+       
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
