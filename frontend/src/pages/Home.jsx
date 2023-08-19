9f594b1ca8c89373c8604a128e60f26b72fbefa6
josephpeter231 <110652580+josephpeter231@users.noreply.github.com>
2023-08-19 16:07:31 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import  { useState } from 'react';
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <>
-      <Navbar/>
-          <div className='bg-red-600' >
-            Home
+    <nav className="bg-gray-800 p-4">
+      <div className="flex items-center justify-between p-4 ">
+        <div className="text-white text-xl">My Website</div>
+        <div className="hidden md:flex space-x-4">
+          <div className="text-white cursor-pointer hover:text-gray-300">Home</div>
+          <div className="text-white cursor-pointer hover:text-gray-300">Event</div>
+          <div className="text-white cursor-pointer hover:text-gray-300">About Us</div>
+          <div className="text-white cursor-pointer hover:text-gray-300">Contact Us</div>
+          <div className="text-white cursor-pointer hover:text-gray-300">Sponsors</div>
+        </div>
+        <button className="md:hidden text-white" onClick={toggleMenu}>
+          Menu
+        </button>
+      </div>
+      {isOpen && (
+        <div className="fixed top-0 right-0 h-full w-64 bg-gray-800 transition-transform transform translate-x-0 md:hidden">
+          <div className="text-center">
+            <div className="text-white cursor-pointer hover:text-gray-300">Home</div>
+            <div className="text-white cursor-pointer hover:text-gray-300">Event</div>
+            <div className="text-white cursor-pointer hover:text-gray-300">About Us</div>
+            <div className="text-white cursor-pointer hover:text-gray-300">Contact Us</div>
+            <div className="text-white cursor-pointer hover:text-gray-300">Sponsors</div>
           </div>
+          <button className="block mx-auto mt-4 text-white" onClick={toggleMenu}>
+            Close
+          </button>
+        </div>
+      )}
+    </nav>
+  );
+};
 
-
-
-
-    </>
-  )
-}
-
-export default Home
+export default Home;
